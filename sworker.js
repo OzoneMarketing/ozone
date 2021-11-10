0ba8490f6a1a9ba687b1f791f3e961731f2af178
OzoneMarketing <93073964+OzoneMarketing@users.noreply.github.com>
2021-11-10 17:38:28 +0530

    Update sworker.js

--- sworker.js
+++ sworker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
    e.waitUntil(
        caches.open("static").then(caches=>{
-           return caches.addAll(["./","./bootstrap-5.0.2-dist/css/bootstrap.min.css","./bootstrap-5.0.2-dist/js/bootstrap.min.js","./style.css","./gallery/LOGO.png","./gallery/bath.png","./gallery/long-body.png","./gallery/short-body.png"]);
+           return caches.addAll(["./","./bootstrap-5.0.2-dist/css/bootstrap.min.css","./bootstrap-5.0.2-dist/js/bootstrap.min.js","./style.css","./gallery/LOGO.png","./gallery/bath.jpg","./gallery/long-body.png","./gallery/short-body.png"]);
        })
    )
 })
