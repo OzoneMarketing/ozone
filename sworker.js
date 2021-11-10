aaace82094d43057ec936a426a7622a750a4dcc7
OzoneMarketing <93073964+OzoneMarketing@users.noreply.github.com>
2021-11-10 17:23:58 +0530

    Add files via upload

---
+++ sworker.js
@@ -0,0 +1,14 @@
+self.addEventListener("install",e=>{
+   e.waitUntil(
+       caches.open("static").then(caches=>{
+           return caches.addAll(["./","./bootstrap-5.0.2-dist/css/bootstrap.min.css","./bootstrap-5.0.2-dist/js/bootstrap.min.js","./style.css","./gallery/LOGO.png","./gallery/bath.png","./gallery/long-body.png","./gallery/short-body.png"]);
+       })
+   )
+})
+self.addEventListener("fetch",e=>{
+    e.respondWidth(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    )
+})
